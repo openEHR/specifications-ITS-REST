d4fcf694f332835d5334a71ab9afe0ca0e5b139a
wolandscat <thomas.beale@openehr.org>
2020-05-27 18:01:06 +0100

    Remove unnecessary script.

--- do_uml_generate-spec-ITS-REST.sh
+++
@@ -1,4 +0,0 @@
-rm docs/UML/classes/*.*
-rm docs/UML/diagrams/*.*
-
-../specifications-AA_GLOBAL/bin/uml_generate.sh -i {its_rest_release} -r ITS-REST -o docs/UML computable/UML/openEHR_UML-ITS-REST.mdzip
