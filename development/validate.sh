c1973883cedfdcec8e83acd55b9364cf70094f05
Sebastian Iancu <sebastian-iancu@users.noreply.github.com>
2023-06-27 18:11:51 +0200

    updating docker-compose to cover most of the docker configuration cases used in the bundle, validate and watch scripts

--- development/validate.sh
+++ development/validate.sh
@@ -1,14 +1,15 @@
-cd ..
 
 function validate() {
-  echo "Validating specifications/"$1".openapi.yaml with openapi-cli.."
-  docker run --rm -u 1000 -v "$(pwd):/spec" redocly/cli lint specifications/"$1".openapi.yaml
-  echo "Validating computable/OAS/"$1"-(validation|codegen).openapi.yaml with openapi-cli.."
-  docker run --rm -u 1000 -v "$(pwd):/spec" redocly/cli lint computable/OAS/"$1"-validation.openapi.yaml
-  docker run --rm -u 1000 -v "$(pwd):/spec" redocly/cli lint computable/OAS/"$1"-codegen.openapi.yaml
+  echo "Validating specifications/"$1".openapi.yaml with redocly.."
+  docker-compose run --rm redocly lint specifications/"$1".openapi.yaml
+  echo
+  echo "Validating computable/OAS/"$1"-(validation|codegen).openapi.yaml with redocly.."
+  docker-compose run --rm redocly lint computable/OAS/"$1"-validation.openapi.yaml
+  docker-compose run --rm redocly lint computable/OAS/"$1"-codegen.openapi.yaml
+  echo
   echo "Validating computable/OAS/"$1"-(validation|codegen).openapi.yaml with swagger.."
-  docker run --rm -u 1000 -v "$(pwd):/data" swagger-cli validate computable/OAS/"$1"-validation.openapi.yaml
-  docker run --rm -u 1000 -v "$(pwd):/data" swagger-cli validate computable/OAS/"$1"-codegen.openapi.yaml
+  docker-compose run --rm swagger-cli validate computable/OAS/"$1"-validation.openapi.yaml
+  docker-compose run --rm swagger-cli validate computable/OAS/"$1"-codegen.openapi.yaml
 }
 
 
@@ -23,10 +24,12 @@ case "${1:-}" in
 		  validate definition
       ;;
     data)
+      cd ..
       SPEC="${2:-ehr}"
       docker run --rm -p 8000:8000 -v "$(pwd):/data" danielgtaylor/apisprout --validate-server --validate-request --watch /data/computable/OAS/"$SPEC"-validation.openapi.yaml
       ;;
     data2)
+      cd ..
       SPEC="${2:-ehr}"
       docker run --init --rm -v "$(pwd):/data" -p 8000:4010 stoplight/prism:4 mock -h 0.0.0.0 /data/computable/OAS/"$SPEC"-validation.openapi.yaml --errors
       ;;
--- development/watch.sh
+++ development/watch.sh
@@ -1,8 +1,6 @@
-cd ../specifications
-
 case "${1:-}" in
 		overview|ehr|query|definition)
-			docker run -d --rm -v "$(pwd):/spec" -p 80:80 -p 32201:32201 redocly/cli preview-docs "$1".openapi.yaml -h 0.0.0.0 -p 80
+			docker-compose run -d --rm -w "/spec/specifications" -p 80:80 -p 32201:32201 redocly preview-docs "$1".openapi.yaml -h 0.0.0.0 -p 80
 			;;
 		"")
 			echo "Usage: watch.sh [overview|ehr|query|definition]"
